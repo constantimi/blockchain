1299611827af2c9197f31d9afd3a9e2abdb06dd1
Konstantin Mihaylov <konstantin.mihaylov.io@gmail.com>
2025-02-02 18:15:28 +0100

    Add TypeScript build files and npm to .gitignore. (48)

--- blockchain-pow.ts
+++ blockchain-pow.ts
@@ -1,13 +1,12 @@
 import * as crypto from 'crypto';
 
 class Transaction {
-
     constructor(
         public amount: number, // amount of the transaction denominated in bitcoin
         public payer: string, // person paying the money
         public payee: string // person receiving the money
-    ) { }
-    
+    ) {}
+
     // Converts the objects to a string
     toString() {
         return JSON.stringify(this);
@@ -16,15 +15,14 @@ class Transaction {
 
 // Block can be represented as a linked list
 class Block {
-
     // Proof of Work concept
-    public nonce = Math.round(Math.random() * 999999999)
-    
+    public nonce = Math.round(Math.random() * 999999999);
+
     constructor(
         public prevHash: string, // link to the previous block
         public transaction: Transaction,
         public ts = Date.now()
-    ) { }
+    ) {}
 
     get hash() {
         const str = JSON.stringify(this);
@@ -42,7 +40,9 @@ class Chain {
     constructor() {
         // Create th Genesis Block
         console.log(`Blockchain created the Genesis Block!`);
-        this.chain = [new Block(null as any, new Transaction(100, 'genesis', 'bitcoin'))]
+        this.chain = [
+            new Block(null as any, new Transaction(100, 'genesis', 'bitcoin')),
+        ];
     }
 
     get lastBlock() {
@@ -52,17 +52,16 @@ class Chain {
     // Proof of work system
     mine(nonce: number) {
         let solution = 1;
-        console.log('⛏️  mining...')
+        console.log('⛏️  mining...');
 
         // Brute force going digit by digit to find the requested value
-        while(true) {
-
+        while (true) {
             const hash = crypto.createHash('MD5'); // Similar such as SHA256, faster to compute
             hash.update((nonce + solution).toString()).end();
 
             const attempt = hash.digest('hex');
 
-            if(attempt.substring(0,4) === '0000'){
+            if (attempt.substring(0, 4) === '0000') {
                 console.log(`Solved: ${solution}`);
                 return nonce + solution;
             }
@@ -71,13 +70,17 @@ class Chain {
         }
     }
 
-    addBlock(transaction: Transaction, senderPublicKey: string, signature: Buffer) {
+    addBlock(
+        transaction: Transaction,
+        senderPublicKey: string,
+        signature: Buffer
+    ) {
         // Verify the transaction
         const verified = crypto.createVerify('SHA256');
         verified.update(transaction.toString());
 
         const isValid = verified.verify(senderPublicKey, signature);
-        
+
         if (isValid) {
             // Add the new Block
             const newBlock = new Block(this.lastBlock.hash, transaction);
@@ -101,7 +104,7 @@ class Wallet {
     constructor() {
         // The `generateKeyPairSync` method accepts two arguments 'type' key and 'options'
         // https://www.geeksforgeeks.org/node-js-crypto-generatekeypairsync-method/
-        const keypair = crypto.generateKeyPairSync("rsa", {
+        const keypair = crypto.generateKeyPairSync('rsa', {
             // The standard secure default length for RSA keys is 2048 bits
             modulusLength: 2048,
             publicKeyEncoding: { type: 'spki', format: 'pem' },
@@ -119,19 +122,26 @@ class Wallet {
     }
 
     sendMoney(amount: number, payee: Wallet, payeePublicKey: string) {
-        
         // Validate the Wallet amount
-        if(amount <= 0) {
-            console.log("RangeError: The amount money which you are sending is't enough to proceed...");
+        if (amount <= 0) {
+            console.log(
+                "RangeError: The amount money which you are sending is't enough to proceed..."
+            );
             return null;
         }
 
-        if(this.amountMoney < amount) {
-            console.log("RangeError: The amount money in the wallet is't enough to proceed...");
+        if (this.amountMoney < amount) {
+            console.log(
+                "RangeError: The amount money in the wallet is't enough to proceed..."
+            );
             return null;
         }
 
-        const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+        const transaction = new Transaction(
+            amount,
+            this.publicKey,
+            payeePublicKey
+        );
 
         const sign = crypto.createSign('SHA256');
         sign.update(transaction.toString()).end();
@@ -140,10 +150,14 @@ class Wallet {
         const signature = sign.sign(this.privateKey);
 
         // Add the block to the BlockChain
-        var result = Chain.instance.addBlock(transaction, this.publicKey, signature);
+        var result = Chain.instance.addBlock(
+            transaction,
+            this.publicKey,
+            signature
+        );
 
         // Add money after validation of the sifnature
-        if(result == true){
+        if (result == true) {
             this.addMoney(payee, amount);
         }
     }
@@ -152,9 +166,8 @@ class Wallet {
         // Add Money to the account
         wallet.amountMoney += amount;
         console.log(`${amount} added to the wallet`);
-    }   
+    }
 }
-
 
 // Example of usage
 var satoshi = new Wallet();
